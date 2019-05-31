2e779e14bf7db2deae33041884c08be7664ffa46
Peter Johansson <pidd69@gmail.com>
2019-05-31 14:40:04 +0200

    Stupid coach done- tests works

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,16 +1,17 @@
-# hddhdhdh
+# FUCK OFF RUBOCOP
+# frozen_string_litteral = true
 class QuestionsController < ApplicationController
   def ask
   end
 
   def answer
     @question = params[:question]
-    if @question[-1] == '?'
-      @answer = 'Silly question, get dressed and go to work!'
-    elsif @question.casecmp?('I am going to work')
-      @answer = 'Great!'
-    else
-      @answer = "I don't care, get dressed and go to work!"
-    end
+    @answer = if @question[-1] == '?'
+                'Silly question, get dressed and go to work!'
+              elsif @question.casecmp?('I am going to work')
+                'Great!'
+              else
+                "I don't care, get dressed and go to work!"
+              end
   end
 end
